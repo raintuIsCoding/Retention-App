ccfd01b64d5d78e2dcde8011d864f4b010514ff1
Robert Umali <Robert.Umali@colorado.edu>
2026-01-04 01:43:28 -0700

    bruh

--- +ret/defaults.m
+++ +ret/defaults.m
@@ -24,6 +24,14 @@ S.MEOP_psi   = 650;   % psi
 S.DF_casing = 2;
 S.DF_pin    = 1.75;
 
+S.DF_casing_from_CLT = 1; % 3.4;
+
+S.theta_scale     = deg2rad(50);
+S.theta_ref = atan(sqrt(2));    % 54.7356 deg
+
+S.hoop_scaling_from_theta  = 1; % (sin(S.theta_scale).^2) ./ (sin(S.theta_ref).^2);
+S.axial_scaling_from_theta = 1; %(cos(S.theta_scale).^2) ./ (cos(S.theta_ref).^2);
+
 % Pattern toggle
 S.pinPatternMode = "progressive";   % "progressive" or "alternating"
 S.altStartPhase  = 0;               % 0 or 1
@@ -32,11 +40,11 @@ S.allowedPinDias = [0.25, 0.3125, 0.375, 0.5];  % in
 
 %% ---------- TARGET STRESS LIMITS ---------- (All set by MIII max pressure motor pressure)
 S.targets.shearOut_max    = 2.45;    % KSI
-S.targets.netTension_max  = 10.83;    % KSI
+S.targets.netTension_max  = 10.82;    % KSI
 S.targets.pinShear_max    = 21.84;          % KSI
 S.targets.bearing_max     = 24.5;    % KSI
-S.targets.hoop_max        = 20.17;        % KSI
-S.targets.axial_max       = 10.09;        % KSI
+S.targets.hoop_max        = 20.17; % * (S.DF_casing_from_CLT/S.DF_casing) * S.hoop_scaling_from_theta;        % KSI
+S.targets.axial_max       = 10.09; % * (S.DF_casing_from_CLT/S.DF_casing) * S.axial_scaling_from_theta;        % KSI
 S.targets.pinShearFOS_min = 1.43;    % Minimum FOS
 
 %% ---------- CASING PARAMETERS ----------
